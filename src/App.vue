9fbe1e662d5eb9e2b15d26585824c372be5b1fea
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2025-02-13 13:50:12 -0300

    App

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,73 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref, type Ref, computed } from 'vue'
+import type { Transaction } from '@/types'
+import { useToast } from 'vue-toastification'
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+
+const toast = useToast()
+const transactions: Ref<Transaction[]> = ref([])
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => acc + transaction.amount, 0)
+})
+
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2)
+})
+
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0)
+    .toFixed(2)
+})
+
+function onTransaction(data: { text: string; amount: number }) {
+  transactions.value.push({
+    id: crypto.randomUUID(),
+    ...data,
+  })
+
+  saveTransactionsToLocalStorage()
+
+  toast.success('Transaction added successfully!')
+}
+
+function onDeleteTransaction(id: string) {
+  transactions.value = transactions.value.filter((transaction) => transaction.id !== id)
+
+  saveTransactionsToLocalStorage()
+
+  toast.success('Transaction deleted successfully!')
+}
+
+function saveTransactionsToLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
+}
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Header />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <Balance :total="total" />
+    <IncomeExpenses :income="income" :expenses="expenses" />
+    <TransactionList :transactions="transactions" @on-delete-transaction="onDeleteTransaction" />
+    <AddTransaction @on-transaction="onTransaction" />
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
